9595438ee5048c7e4dce2fb5bdb731a934c0e297
Jorge Guedes <jorgeguedes16@gmail.com>
2023-11-02 17:37:41 +0000

    Abstracted operations into their on classes

--- calculator.py
+++ calculator.py
@@ -1,8 +1,45 @@
 ''' RPN Calculator '''
+import math
 
-class Calculator:
+class Operation:
+    def __init__ (self, operator):
+        self.operator = operator
+
+    def operate (self, stack):
+        pass
+
+
+class Addition(Operation):
+    def operate (self, stack):
+        return stack.pop() + stack.pop()
+
+class Subtract(Operation):
+    def operate (self, stack):
+        return stack.pop() - stack.pop()
+
+class Divide(Operation):
+    def operate (self, stack):
+        denominator = stack.pop()
+        return stack.pop() / denominator
+
+class Multiply(Operation):
+    def operate (self, stack):
+        return stack.pop() * stack.pop()
+
+class SquareRoot(Operation):
+    def operate (self, stack):
+        return math.sqrt(stack.pop())
+
+class Stack:
     def __init__ (self):
         self.stack = []
+        self.operations = [
+            Addition("+"),
+            Subtract("-"),
+            Divide("/"),
+            Multiply("*"),
+            SquareRoot("sqrt")
+        ]
 
     def push(self, value):
         self.stack.append(value) 
@@ -12,46 +49,25 @@ class Calculator:
         return self.stack.pop()  
 
 
-    def add(self):
-        return self.pop() + self.pop()
-
-
-    def subtract(self):
-        return self.pop() - self.pop()
-
-
-    def divide(self):
-        denominator = self.pop()
-        return self.pop() / denominator
-
-
-    def multiply(self):
-        return self.pop() * self.pop()
-
-
     def calculate(self, operator):
-        if operator == "+":
-            return self.add()
-        elif operator == "-":
-            return self.subtract()
-        elif operator == "/":
-            return self.divide()
-        elif operator == "*":
-            return self.multiply()
+        for op in self.operations:
+            if op.operator == operator:
+                return op.operate(self.stack)
 
-calculator = Calculator() #object
+
+stack = Stack() #object
 
 while True:
-    val_oper = input("Digit or + - / * ")
+    val_oper = input("> ")
 
     if val_oper.isnumeric():
-        calculator.push(int(val_oper))
+        stack.push(int(val_oper))
 
     else:
-        calculator.push(calculator.calculate(val_oper))
+        stack.push(stack.calculate(val_oper))
 
         """
         if not calculate(val_oper):
             break"""
 
-    print(calculator.stack)
+    print(stack.stack)
